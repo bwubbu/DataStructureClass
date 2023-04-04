837900238f3f6504deadf90582faaef5f32da4d7
bwubbu <kyrodahero123@gmail.com>
2023-04-04 15:30:17 +0800

    added L3Q1

---
+++ Lab/src/L3/BidCollectionInterface.java
@@ -0,0 +1,65 @@
+package L3;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ * An ADT representing a collection of bids for installing air conditioning units.
+ */
+public interface BidCollectionInterface {
+
+    /**
+     * Adds a bid to this collection.
+     * @param bid the bid to add
+     */
+    void addBid(BidInterface bid);
+
+    /**
+     * Returns the bid in this collection with the best yearly cost.
+     * @return the bid with the best yearly cost
+     * @throws NoSuchElementException if the collection is empty
+     */
+    BidInterface getBestYearlyCostBid() throws NoSuchElementException;
+
+    /**
+     * Returns the bid in this collection with the best initial cost.
+     * The initial cost will be defined as the unit cost plus the installation cost.
+     * @return the bid with the best initial cost
+     * @throws NoSuchElementException if the collection is empty
+     */
+    BidInterface getBestInitialCostBid() throws NoSuchElementException;
+
+    /**
+     * Clears all of the items from this collection.
+     */
+    void clear();
+
+    /**
+     * Gets the number of items in this collection.
+     * @return the number of items in the collection
+     */
+    int size();
+
+    /**
+     * Sees whether this collection is empty.
+     * @return true if the collection is empty; false otherwise
+     */
+    boolean isEmpty();
+
+    /**
+     * Searches for bids with yearly operating costs within a specified range.
+     * @param low the low end of the range (inclusive)
+     * @param high the high end of the range (inclusive)
+     * @return a list of bids with yearly operating costs in the specified range
+     */
+    List<BidInterface> searchByYearlyCost(double low, double high);
+
+    /**
+     * Searches for bids with SEER ratings within a specified range.
+     * @param low the low end of the range (inclusive)
+     * @param high the high end of the range (inclusive)
+     * @return a list of bids with SEER ratings in the specified range
+     */
+    List<BidInterface> searchBySEER(double low, double high);
+
+}
---
+++ Lab/src/L3/BidInterface.java
@@ -0,0 +1,50 @@
+package L3;
+
+/**
+ * An ADT representing a single bid for installing an air conditioning unit.
+ */
+public interface BidInterface {
+
+    /**
+     * Returns the name of the company making this bid.
+     * @return the name of the company
+     */
+    String getCompanyName();
+
+    /**
+     * Returns the description of the air conditioner that this bid is for.
+     * @return the description of the AC
+     */
+    String getAcDescription();
+
+    /**
+     * Returns the capacity of this bid's AC in tons (1 ton = 12,000 BTU).
+     * @return the AC capacity in tons
+     */
+    double getAcCapacity();
+
+    /**
+     * Returns the seasonal efficiency of this bid's AC (SEER).
+     * @return the AC SEER
+     */
+    double getAcSEER();
+
+    /**
+     * Returns the cost of this bid's AC.
+     * @return the AC cost
+     */
+    double getAcCost();
+
+    /**
+     * Returns the cost of installing this bid's AC.
+     * @return the installation cost
+     */
+    double getInstallationCost();
+
+    /**
+     * Returns the yearly cost of operating this bid's AC.
+     * @return the yearly operating cost
+     */
+    double getYearlyOperatingCost();
+
+}
